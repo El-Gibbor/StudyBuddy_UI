f276b3c72938a51eee08560ac019769504a39306
El-gibbor <chiagoziem11@yahoo.com>
2025-07-06 20:07:53 +0200

    - Component rendering: landing page

--- study_buddy/src/App.jsx
+++ study_buddy/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import LandingPage from './components/LandingPage';
+
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
+      <main className="flex-grow">
+        <LandingPage />
+      </main>
     </div>
+
   );
 }
 
